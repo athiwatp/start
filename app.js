b91353ae73367d4bd25f504a6857acd6272365a0
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-09-14 19:22:09 +0700

    Insert new user

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var ejs = require('ejs');
+var mongo = require('mongodb');
 app.listen(2000);
 app.engine('html', ejs.renderFile);
 
@@ -12,8 +13,16 @@ app.get('/register', function(req, res) {
 
 app.post('/register', registerNewUser);
 
-// install packages: npm install express ejs
+// install packages: npm install express ejs mongodb
 // run:              node app
+
+// mongodb on Windows:
+// cd /Users/xxx/Desktop/mongo/bin
+// mongod --dbpath . --storageEngine=mmapv1
+
+// mongodb on macOS
+// cd /Users/xxx/Dektop/mongo/bin
+// ./mongod --dbpath .
 
 function registerNewUser(req, res) {
 	var data = "";
@@ -22,9 +31,18 @@ function registerNewUser(req, res) {
 		data = decodeURIComponent(data);
 		data = data.replace(/\+/g, ' ');
 		var a = data.split('&');
+		var u = { };
 		for (var i = 0; i < a.length; i++) {
-			
+			var f = a[i].split('=');
+			u[f[0]] = f[1];
 		}
+
+		mongo.MongoClient.connect('mongodb://127.0.0.1/start',
+			(error, db) => {
+				db.collection('user').insert(u);
+			}
+		)
+
 		res.redirect("/");
 	});
 }
