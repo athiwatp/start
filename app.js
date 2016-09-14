ca71ad4f641b8d809d467a66d8dbedd11ff1288e
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-09-14 18:40:11 +0700

    Update

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 var express = require('express');
 var app = express();
+var ejs = require('ejs');
 app.listen(2000);
+app.engine('html', ejs.renderFile);
+
+app.get('/', (req, res) => res.render('index.html') );
+
+app.get('/register', function(req, res) {
+	res.render('register.html');
+});
+
+
+
+// install packages: npm install express ejs
+// run:              node app
