bd6400da4bee31aa374f1c5c65f8d94adb10f9ba
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-09-14 19:02:16 +0700

    Read data from POST

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ app.get('/register', function(req, res) {
 	res.render('register.html');
 });
 
-
+app.post('/register', registerNewUser);
 
 // install packages: npm install express ejs
 // run:              node app
+
+function registerNewUser(req, res) {
+	var data = "";
+	req.on("data", chunk => data += chunk )
+	req.on("end", () => {
+		data = decodeURIComponent(data);
+		data = data.replace('+', ' ');
+		console.log(data);
+		res.redirect("/");
+	});
+}
